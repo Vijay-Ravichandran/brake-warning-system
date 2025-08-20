93d21d643b4eb90e86d3ac4898f4db9477d0adb4
Vijayaragavan R <vijayravi.0906@gmail.com>
2025-08-20 14:27:14 +0530

    Adding function logic unit

---
+++ brake_light.py
@@ -0,0 +1,12 @@
+def check_brake_fluid(fluid_percent):
+    if fluid_percent <0 or fluid_percent >100:
+        return "ERROR"
+    if fluid_percent <20:
+        return "ON"
+    else: 
+        return "OFF"
+print(check_brake_fluid(15))
+print(check_brake_fluid(-1))
+print(check_brake_fluid(101))
+print(check_brake_fluid(75))
+dir
